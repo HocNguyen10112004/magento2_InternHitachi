2bbc7b6e8b3cff6a6fda9215fc8606f99564133b
HocNguyen <hocnguyen@example.com>
2025-04-11 09:11:10 +0700

    l

--- app/code/Week1/Eventobserver/Controller/Index/Index.php
+++ app/code/Week1/Eventobserver/Controller/Index/Index.php
@@ -5,11 +5,19 @@ namespace Week1\Eventobserver\Controller\Index;
 class Index extends \Magento\Framework\App\Action\Action
 {
 
+	// public function execute()
+	// {
+	// 	$textDisplay = new \Magento\Framework\DataObject(array('text' => 'Mageplaza'));
+	// 	$this->_eventManager->dispatch('week1_eventobserver_display_text', ['mp_text' => $textDisplay]);
+	// 	echo $textDisplay->getText();
+	// 	exit;
+	// }
 	public function execute()
 	{
-		$textDisplay = new \Magento\Framework\DataObject(array('text' => 'Mageplaza'));
+		$textDisplay = 'Mageplaza';
 		$this->_eventManager->dispatch('week1_eventobserver_display_text', ['mp_text' => $textDisplay]);
-		echo $textDisplay->getText();
+		echo $textDisplay; // vẫn in ra "Mageplaza"
 		exit;
 	}
+
 }
--- app/code/Week1/Eventobserver/Observer/ChangeDisplayText.php
+++ app/code/Week1/Eventobserver/Observer/ChangeDisplayText.php
@@ -8,10 +8,17 @@ class ChangeDisplayText implements ObserverInterface
 {
     public function execute(\Magento\Framework\Event\Observer $observer)
 	{
-		$displayText = $observer->getData('mp_text');
-		echo $displayText->getText() . " - Event </br>";
-		$displayText->setText('Execute event successfully.');
+		// $displayText = $observer->getData('mp_text');
+		// echo $displayText->getText() . " - Event </br>";
+		// $displayText->setText('Execute event successfully.');
 
-		return $this;
+		// return $this;
+		$displayText = $observer->getData('mp_text');
+
+        echo $displayText . " - In Observer<br>"; // In ra giá trị nhận được
+
+        $displayText = "Modified by Observer"; // Chỉ thay đổi bản sao, không ảnh hưởng gì đến biến gốc
+
+        return $this;
 	}
 }
